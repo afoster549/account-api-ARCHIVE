284c21bf78f4b600da499e144a85c364fe470016
123-Game <aefoster74@gmail.com>
2023-09-30 01:28:12 +0100

    Added session response

--- routes/v1/sessions.js
+++ routes/v1/sessions.js
@@ -2,16 +2,18 @@ const express = require("express")
 const router = express.Router()
 const auth_mid = require("../../middleware/auth")
 
-const bcrypt = require("bcrypt")
-
 const user_model = require("../../models/user")
 
 router.use(auth_mid)
 
 router.post("/", async (req, res) => {
     try {
-        res.status(200).send({
-            message: "Test"
+        const data  = await user_model.findOne({ token: req.body.token })
+
+        const sessions = JSON.parse(data.sessions)
+
+        res.status(200).json({
+            data: sessions
         })
     } catch {
         res.status(500).json({
