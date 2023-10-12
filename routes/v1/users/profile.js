d729cf25a4e6bf63148a613abb077eeb5d2659c2
123-Game <aefoster74@gmail.com>
2023-10-12 19:41:51 +0100

    Updated profile.js

---
+++ routes/v1/users/profile.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const router = express.Router()
+
+const user_model = require("../../models/user")
+
+router.post("/", async (req, res) => {
+    try {
+        if (typeof(req.body.userId) === "undefined" && typeof(req.body.username) === "undefined") {
+            res.status(406).json({
+                error: "No id or username provided."
+            })
+        } else {
+            let user
+
+            if (typeof(req.body.username) === "undefined") {
+                user = await user_model.findOne({ lower_username: req.body.username.toLowerCase() })
+            } else {
+                user = await user_model.findOne({ id: req.body.id.toLowerCase() })
+            }
+
+            res.status(200).json({
+                data: {
+                    nickname: user.nickname,
+                    username: user.username,
+                    badges: user.badges
+                }
+            })
+        }
+    } catch {
+        res.status(500).json({
+            message: "Something went wrong."
+        })
+    }
+})
+
+module.exports = router
