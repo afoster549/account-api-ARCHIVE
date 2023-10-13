c25d6698064bc95d352f1ecb111501bbdafcde0f
123-Game <aefoster74@gmail.com>
2023-10-13 17:36:58 +0100

    Updated updateavatar.js

---
+++ routes/v1/account/updateavatar.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const router = express.Router()
+
+const user_model = require("../../models/user")
+
+router.post("/", async (req, res) => {
+    try {
+        if (typeof(req.body.token) === "undefined" && typeof(req.body.session) === "undefined" && typeof(req.body.avatarUrl) === "undefined") {
+            res.status(406).json({
+                error: "No token or session provided."
+            })
+        } else {
+            const data = await user_model.findOne({ token: req.body.token })
+
+            const sessions = JSON.parse(data.sessions)
+
+            if (sessions[req.body.session]) {
+                data.avatarUrl = req.body.avatarUrl
+
+                data.save()
+            } else {
+                res.status(500).json({
+                    message: "Something went wrong."
+                })
+            }
+
+            res.status(200).json({
+                data: "Updated avatar"
+            })
+        }
+    } catch {
+        res.status(500).json({
+            message: "Something went wrong."
+        })
+    }
+})
+
+module.exports = router
