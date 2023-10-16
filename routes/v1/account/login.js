37f233858ea3e32d808881fe80e141ec296082c6
123-Game <aefoster74@gmail.com>
2023-10-16 16:34:10 +0100

    Changed error JSON

--- routes/v1/account/login.js
+++ routes/v1/account/login.js
@@ -76,7 +76,7 @@ router.post("/", async (req, res) => {
         }
     } catch {
         res.status(500).json({
-            message: "Something went wrong."
+            error: "Something went wrong."
         })
     }
 })
