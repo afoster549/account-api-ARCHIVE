2a54e192b2e093f1b1050903d67c2ba7b3809623
123-Game <aefoster74@gmail.com>
2023-10-17 22:40:24 +0100

    Changed validation for token

--- routes/v1/account/sessions.js
+++ routes/v1/account/sessions.js
@@ -4,23 +4,27 @@ const auth_mid = require("../../../middleware/auth")
 
 const user_model = require("../../../models/user")
 
+const validation = require("../../../middleware/validation")
+
+router.use(
+    validation({
+        "token": {
+            type: "string"
+        }
+    }, false)
+)
+
 router.use(auth_mid)
 
 router.post("/", async (req, res) => {
     try {
-        if (typeof(req.body.token) != "string") {
-            res.status(406).json({
-                error: "No token provided."
-            })
-        } else {
-            const data  = await user_model.findOne({ token: req.body.token })
+        const data = await user_model.findOne({ token: req.body.token })
 
-            const sessions = JSON.parse(data.sessions)
+        const sessions = JSON.parse(data.sessions)
 
-            res.status(200).json({
-                data: sessions
-            })
-        }
+        res.status(200).json({
+            data: sessions
+        })
     } catch {
         res.status(500).json({
             error: "Something went wrong."
