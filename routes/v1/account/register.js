bb2c8040775dfa4aabcde0f1c5200cdbe1e9558e
123-Game <aefoster74@gmail.com>
2023-10-17 22:39:22 +0100

    Changed validation for username password and email

--- routes/v1/account/register.js
+++ routes/v1/account/register.js
@@ -5,13 +5,29 @@ const bcrypt = require("bcrypt")
 
 const user_model = require("../../../models/user")
 
+const validation = require("../../../middleware/validation")
+
+router.use(
+    validation({
+        "username": {
+            type: "string"
+        },
+        "password": {
+            type: "string"
+        },
+        "email": {
+            type: "string"
+        }
+    }, false)
+)
+
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 
 function gen_token() {
     let start = ""
     const mid = Buffer.from(Date.now().toString()).toString("base64")
     let end = ""
-    
+
     for (let i = 0; i < 50; i++) {
         start += characters.charAt(Math.floor(Math.random() * characters.length))
     }
@@ -35,63 +51,52 @@ function validate_password(password) {
 
 router.post("/", async (req, res) => {
     try {
-        if (typeof(req.body.username) != "string" || typeof(req.body.password) != "string" || typeof(req.body.email) != "string") {
-            res.status(400).json({
-                error: "Invalid JSON format."
-            })
-        } else {
-            let errors = {}
-    
-            const passwordValidation = validate_password(req.body.password)
-    
-            if (passwordValidation) {
-                errors.password = passwordValidation
+        let errors = {}
+
+        const passwordValidation = validate_password(req.body.password)
+
+        if (passwordValidation) {
+            errors.password = passwordValidation
+        }
+
+        const token = gen_token()
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+        const doc = new user_model({
+            userId: await user_model.count({}),
+            token: token,
+            username: req.body.username,
+            lower_username: req.body.username.toLowerCase(),
+            nickname: req.body.username,
+            password: hashedPassword,
+            email: req.body.email,
+            regip: req.headers["x-forwarded-for"] || req.socket.remoteAddress
+        })
+
+        try {
+            const error = doc.validateSync()
+
+            if (error.errors.username) {
+                errors.username = error.errors.username.properties.message
             }
-            
-            try {
-                const token = gen_token()
-                const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    
-                const doc = new user_model({
-                    userId: await user_model.count({}),
-                    token: token,
-                    username: req.body.username,
-                    lower_username: req.body.username.toLowerCase(),
-                    nickname: req.body.username,
-                    password: hashedPassword,
-                    email: req.body.email,
-                    regip: req.headers["x-forwarded-for"] || req.socket.remoteAddress
+
+            if (error.errors.email) {
+                errors.email = error.errors.email.properties.message
+            }
+
+            res.status(500).json({
+                error: errors
+            })
+        } catch {
+            if (Object.keys(errors).length === 0) {
+                doc.save()
+
+                res.status(201).json({
+                    message: "Account created."
                 })
-    
-                try {
-                    const error = doc.validateSync()
-    
-                    if (error.errors.username) {
-                        errors.username = error.errors.username.properties.message
-                    }
-                    if (error.errors.email) {
-                        errors.email = error.errors.email.properties.message
-                    }
-    
-                    res.status(500).json({
-                        error: errors
-                    })
-                } catch {
-                    if (Object.keys(errors).length === 0) {
-                        doc.save()
-    
-                        res.status(201).json({
-                            message: "Account created."
-                        })
-                    } else {
-                        res.status(400).json({
-                            error: errors
-                        })
-                    }
-                }
-            } catch {
-                res.status(500).json({
-                    error: "Something went wrong."
+            } else {
+                res.status(400).json({
+                    error: errors
                 })
             }
         }
