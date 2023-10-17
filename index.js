cd5a55a4ad2f72d839f3effd7e0c382dec07c643
123-Game <aefoster74@gmail.com>
2023-10-17 21:55:02 +0100

    Updated routes

--- index.js
+++ index.js
@@ -36,42 +36,42 @@ app.get("/", (req, res) => {
 
 // Register
 
-const register_route = require("./routes/v1/accounts/register")
-app.use("/v1/accounts/register", register_route)
+const register_route = require("./routes/v1/account/register")
+app.use("/v1/account/register", register_route)
 
 // Login
 
-const login_route = require("./routes/v1/accounts/login")
-app.use("/v1/accounts/login", login_route)
+const login_route = require("./routes/v1/account/login")
+app.use("/v1/account/login", login_route)
 
 // Auth
 
-const auth_route = require("./routes/v1/accounts/auth")
-app.use("/v1/accounts/auth", auth_route)
+const auth_route = require("./routes/v1/account/auth")
+app.use("/v1/account/auth", auth_route)
 
 // Gen Password Reset Link
 
-const send_password_reset_link_route = require("./routes/v1/accounts/sendresetlink")
-app.use("/v1/accounts/sendresetlink", send_password_reset_link_route)
+const send_password_reset_link_route = require("./routes/v1/account/sendresetlink")
+app.use("/v1/account/sendresetlink", send_password_reset_link_route)
 
 // Get Password Reset Link
 
-const get_password_reset_link_route = require("./routes/v1/accounts/getresetlink")
-app.use("/v1/accounts/getpasswordresetlink", get_password_reset_link_route)
+const get_password_reset_link_route = require("./routes/v1/account/getresetlink")
+app.use("/v1/account/getpasswordresetlink", get_password_reset_link_route)
 
 // Reset Password
 
-const reset_password_route = require("./routes/v1/accounts/resetpassword")
-app.use("/v1/accounts/resetpassword", reset_password_route)
+const reset_password_route = require("./routes/v1/account/resetpassword")
+app.use("/v1/account/resetpassword", reset_password_route)
 
 // Sessions [auth protected]
 
-const sessions_route = require("./routes/v1/accounts/sessions")
-app.use("/v1/accounts/sessions", sessions_route)
+const sessions_route = require("./routes/v1/account/sessions")
+app.use("/v1/account/sessions", sessions_route)
 
 // Profile
 
-const profile_route = require("./routes/v1/users/profile")
+const profile_route = require("./routes/v1/user/profile")
 app.use("/v1/users/profile", profile_route)
 
 app.listen(port, () => {
