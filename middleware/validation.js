6060d367bb0fc159c1e1f2325b39e7a3b4236d48
123-Game <aefoster74@gmail.com>
2023-10-17 22:37:45 +0100

    Created validation middleware

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,9 +1,26 @@
-class validate {
-    static data(dat) {
-        return (req, res, next) => {
-            console.log(this.data)
+const validate = (data, any) => {
+    return (req, res, next) => {
+        let validated = false
+        let correct = 0
 
+        const keys = Object.keys(data)
+
+        for (let i = 0; i < keys.length; i++) {
+            const item = keys[i]
+
+            if (typeof(req.body[item]) != data[item].type) {
+                validated = false
+            } else {
+                correct++
+            }
+        }
+
+        if (correct === keys.length || correct > 0 && any) {
             next()
+        } else {
+            res.status(406).json({
+                error: "Malformed request."
+            })
         }
     }
 }
