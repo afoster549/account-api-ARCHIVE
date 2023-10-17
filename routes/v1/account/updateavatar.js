68e5475568e192a2b2d5d82c18966cab66daad00
123-Game <aefoster74@gmail.com>
2023-10-17 22:40:10 +0100

    Changed validation for token session and avatarurl

--- routes/v1/account/updateavatar.js
+++ routes/v1/account/updateavatar.js
@@ -3,36 +3,46 @@ const router = express.Router()
 
 const user_model = require("../../../models/user")
 
+const validation = require("../../../middleware/validation")
+
+router.use(
+    validation({
+        "token": {
+            type: "string"
+        },
+        "session": {
+            type: "string"
+        },
+        "avatarurl": {
+            type: "string"
+        }
+    }, false)
+)
+
 router.post("/", async (req, res) => {
     try {
-        if (typeof(req.body.token) != "string" || typeof(req.body.session) != "string" || typeof(req.body.avatarUrl) != "string") {
-            res.status(406).json({
-                error: "No token, session or avatar url provided."
+        if (!req.body.avatarUrl.startsWith("https://cdn.afoster.uk/images/users/avatar/")) {
+            res.status(400).json({
+                error: "Invalid Avatar Url."
             })
         } else {
-            if (!req.body.avatarUrl.startsWith("https://cdn.afoster.uk/images/users/avatar/")) {
-                res.status(400).json({
-                    error: "Invalid Avatar Url."
-                })
-            } else {
-                const data = await user_model.findOne({ token: req.body.token })
+            const data = await user_model.findOne({ token: req.body.token })
 
-                const sessions = JSON.parse(data.sessions)
-    
-                if (sessions[req.body.session]) {
-                    data.avatarUrl = req.body.avatarUrl
-    
-                    data.save()
-                } else {
-                    res.status(500).json({
-                        error: "Something went wrong."
-                    })
-                }
-    
-                res.status(200).json({
-                    data: "Updated avatar."
-                }) 
+            const sessions = JSON.parse(data.sessions)
+
+            if (sessions[req.body.session]) {
+                data.avatarUrl = req.body.avatarUrl
+
+                data.save()
+            } else {
+                res.status(500).json({
+                    error: "Something went wrong."
+                })
             }
+
+            res.status(200).json({
+                data: "Updated avatar."
+            })
         }
     } catch {
         res.status(500).json({
